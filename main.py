448d28f0b3f9d5b2471b6ced669c91a54abcbf2b
Qazi <qazi.husnain878@gmail.com>
2023-06-21 05:43:05 -0700

    return data from data files for temperature

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from flask import Flask,render_template
-app=Flask("Website")
+import pandas as pd
+import numpy as np
+from datetime import datetime
+app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
-    temperature=23
-    tem_dict={"station":station,"date":date,"temp":temperature}
+    filepath="data/TG_STAID"+str(station).zfill(6)+".txt"
+    df=pd.read_csv(filepath,skiprows=20,parse_dates=["    DATE"])
+    df["TG0"] = df["   TG"].mask(df["   TG"] == -9999, np.nan)
+    df["TG1"]=df["TG0"] /10
+
+    date_obj = datetime.strptime(date, "%Y%m%d")
+    formatted_date = date_obj.strftime("%Y-%m-%d")
+
+    temperature=df.loc[df["    DATE"]==formatted_date]["TG1"].squeeze()
+    tem_dict={"station":station,"date":formatted_date,"temp":temperature}
     return tem_dict
-app.run(debug=True)
+if __name__=="__main__":
+    app.run(debug=True)
